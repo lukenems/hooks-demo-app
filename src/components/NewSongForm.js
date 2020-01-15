31454c254bca33465e1079c0b678e7a08814f005
Luke Nemy <lukenemy7@gmail.com>
2020-01-15 15:13:15 -0800

    added artist feature

--- src/components/NewSongForm.js
+++ src/components/NewSongForm.js
@@ -1,16 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const NewSongForm = ({ addSong }) => {
   const [title, setTitle] = useState('')
+  const [artist, setArtist] = useState('')
   const handleSubmit = (e) => {
     e.preventDefault();
-    addSong(title);
-    setTitle('')
+    addSong(artist, title);
+    setArtist('');
+    setTitle('');
   }
   return ( 
     <form onSubmit={handleSubmit}>
-      <label>Song Name:</label>
+      <label>Artist Name</label>
+      <br />
+      <input type="text" value={artist} required onChange={(e) => setArtist(e.target.value)}/>
+      <br />
+      <label>Song Name</label>
+      <br />
       <input type="text" value={title} required onChange={(e) => setTitle(e.target.value)}/>
+      <br/>
       <input type="submit" value='add song'/>
     </form>
    );
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
-import uuid from 'uuid';
-import NewSongForm from './NewSongForm';
+import React, { useState, useEffect } from "react";
+import uuid from "uuid";
+import NewSongForm from "./NewSongForm";
 
 const SongList = () => {
   const [songs, setSongs] = useState([
-    {title: 'Extreme Ways', id: 1},
-    {title: 'Winter to Pry', id: 2},
-    {title: 'Everything I Wanted', id: 3}
+    { artist: 'Moby', title: "Extreme Ways", id: 1 },
+    { artist: 'The Deer', title: "Winter to Pry", id: 2 },
+    { artist: 'Billie Elish', title: "Everything I Wanted", id: 3 }
   ]);
-  const addSong = (title) => {
-    setSongs([...songs, { title, id: uuid() }])
-  }
-  return ( 
-    <div className="song-list">
+
+  const addSong = (artist, title) => {
+    setSongs([...songs, { artist, title, id: uuid() }]);
+  };
+
+  const [age, setAge] = useState(20);
+  
+  useEffect(() => {
+    console.log("quick useEffect test", age);
+  }, [age, songs]);
+
+  return (
+    <div className='song-list'>
       <ul>
         {songs.map(song => {
-          return ( <li key={song.id}>{song.title}</li> );
+          return (
+            <>
+              <h3>{song.artist}</h3>
+              <li key={song.id}>{song.title}</li>
+            </>  
+          );
         })}
       </ul>
-      <NewSongForm addSong={addSong}/>
+      <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 age: {age}</button>
     </div>
-   );
-}
- 
+  );
+};
+
 export default SongList;
