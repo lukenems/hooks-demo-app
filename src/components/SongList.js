69c9418bc467dff9c591107a98c23dee78b841bd
Luke Nemy <lukenemy7@gmail.com>
2020-01-15 13:20:18 -0800

    setup for hooks app

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return ( 
+    <div className="song-list">
+      <ul>
+        <li>Winter to Pry</li>
+        <li>July</li>
+      </ul>
+    </div>
+   );
+}
+ 
+export default SongList;
