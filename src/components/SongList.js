bd51208a1246502697cf6200ff92804d2a15fd5a
Luke Nemy <lukenemy7@gmail.com>
2020-01-15 14:38:15 -0800

    enabled add song feature

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    {title: 'Extreme Ways', id: 1},
+    {title: 'Winter to Pry', id: 2},
+    {title: 'Everything I Wanted', id: 3}
+  ]);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuid() }])
+  }
   return ( 
     <div className="song-list">
       <ul>
-        <li>Winter to Pry</li>
-        <li>July</li>
+        {songs.map(song => {
+          return ( <li key={song.id}>{song.title}</li> );
+        })}
       </ul>
+      <NewSongForm addSong={addSong}/>
     </div>
    );
 }
