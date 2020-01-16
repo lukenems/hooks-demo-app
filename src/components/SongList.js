c20836113b3bd733fb8e77376c13d2bfcc7aa510
Luke Nemy <lukenemy7@gmail.com>
2020-01-15 16:20:51 -0800

    addedd feature to count how many times you listen to an individual song

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,21 +3,31 @@ import uuid from "uuid";
 import NewSongForm from "./NewSongForm";
 
 const SongList = () => {
+
   const [songs, setSongs] = useState([
-    { artist: 'Moby', title: "Extreme Ways", id: 1 },
-    { artist: 'The Deer', title: "Winter to Pry", id: 2 },
-    { artist: 'Billie Elish', title: "Everything I Wanted", id: 3 }
+    { artist: 'Moby', title: "Extreme Ways", id: 1, timesListened: 1 },
+    { artist: 'The Deer', title: "Winter to Pry", id: 2, timesListened: 1 },
+    { artist: 'Billie Elish', title: "Everything I Wanted", id: 3, timesListened: 1 }
   ]);
 
   const addSong = (artist, title) => {
-    setSongs([...songs, { artist, title, id: uuid() }]);
+    setSongs([...songs, { artist, title, id: uuid(), timesListened: 1 }]);
   };
 
-  const [age, setAge] = useState(20);
+  const addListen = listened => {
+    const allSongs = [...songs]
+    for (let i of allSongs) {
+      if (i.title === listened.title) {
+        i.timesListened = i.timesListened + 1
+        break;
+      }
+    }
+    setSongs([...allSongs])
+  }
   
-  useEffect(() => {
-    console.log("quick useEffect test", age);
-  }, [age, songs]);
+  // useEffect(() => {
+  //   console.log("quick useEffect test", timesListened);
+  // }, [timesListened]);
 
   return (
     <div className='song-list'>
@@ -27,12 +37,13 @@ const SongList = () => {
             <>
               <h3>{song.artist}</h3>
               <li key={song.id}>{song.title}</li>
+              <button onClick={() => addListen(song)}>I've listend to this song {song.timesListened} times</button>
             </>  
           );
         })}
       </ul>
       <NewSongForm addSong={addSong} />
-      <button onClick={() => setAge(age + 1)}>Add 1 age: {age}</button>
+      
     </div>
   );
 };
